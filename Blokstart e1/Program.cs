ae1acc11ca27cb8d593b51bc1a96f582761e5a1b
merijn <merijnsweep@gmail.com>
2023-09-07 12:38:14 +0200

    Initial commit

---
+++ Blokstart e1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Blokstart_e1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
