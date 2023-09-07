eef4c900ac5dd4454a6140a362a4db86be3ca0b4
merijn <merijnsweep@gmail.com>
2023-09-07 14:00:21 +0200

    first  commiy

--- Blokstart e1/Program.cs
+++ Blokstart e1/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welkom speler bij Dungeon Dude voer jouw naam in:");
+
+            string pName = Console.ReadLine();
+
+            Console.WriteLine($"{pName} welkom bij Dungeon Dude. ");
         }
     }
 }
