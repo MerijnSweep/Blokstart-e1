ba5f64d9481c7b01d62ccc9d9876e653bedc125f
merijn <merijnsweep@gmail.com>
2023-09-08 14:30:11 +0200

    Intro commit

---
+++ Blokstart e1/Model/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blokstart_e1.Model
+{
+    internal class Game
+    {
+
+    }
+}
--- Blokstart e1/Program.cs
+++ Blokstart e1/Program.cs
@@ -4,11 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welkom speler bij Dungeon Dude voer jouw naam in:");
+           bool IsRunning = true;
+            
+                Console.WriteLine("Welkom speler bij Dungeon Dude voer jouw naam in:");
 
-            string pName = Console.ReadLine();
+                string pName = Console.ReadLine();
 
-            Console.WriteLine($"{pName} welkom bij Dungeon Dude. ");
+                Console.WriteLine($"{pName} welkom bij Dungeon Dude! ");
+            while (IsRunning == true)
+            {
+                Console.WriteLine("How do you play?");
+                Console.WriteLine("On the screen, a description will appear first of what is happening here, below which choices will appear in a numbered list.");
+                Console.WriteLine("You can select these options by typing the corresponding number and pressing enter, which will perform an action.");
+                Console.WriteLine("If the controls are no longer clear, you can type 'help,' and the instructions will be displayed again.");
+                Console.WriteLine("1.   Continue");
+                Console.WriteLine("2.   Stop playing");
+                Console.WriteLine("Select your choice:");
+                Console.ReadLine();
+                string choice = Console.ReadLine();
+                if (choice == "1")
+                {
+                    continue;
+                }
+                else if (choice == "2")
+                {
+                    IsRunning = false;
+                }
+                else
+                {
+                    return;
+                }
+
+            }
         }
     }
 }
