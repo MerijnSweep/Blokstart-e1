cb813c51935b0a2f732641d14816fd3d29cd8e96
merijn <merijnsweep@gmail.com>
2023-09-21 21:09:36 +0200

    ExploreCave + Combat functions  Commit
    maakt de combat functions los van explore forest en maakt nieuwe aan voor de cave en Voegt de Cave optie toe aan het command menu

---
+++ Blokstart e1/CONVENTIONS.md
@@ -0,0 +1,34 @@
+# C# Code Conventie
+
+Dit document beschrijft de codeconventie voor het C#-project [projectnaam].
+
+## Bestandsnaamconventie
+
+- Bestandsnamen moeten in PascalCase zijn en de naam van de klasse of het hoofdbestand weerspiegelen. Bijvoorbeeld: `MyClass.cs`, `Program.cs`.
+
+## Indentatie en Inspringing
+
+- Gebruik **vier spaties** om in te springen.
+- Vermijd het gebruik van tabs voor inspringing.
+
+## Stijlrichtlijnen
+
+- Gebruik **PascalCase** voor klasse- en type-namen. Bijvoorbeeld: `MyClass`, `PersonModel`.
+- Gebruik **camelCase** voor variabelen en parameters. Bijvoorbeeld: `myVariable`, `myParameter`.
+- Gebruik **UpperCamelCase** voor naamgeving van methoden. Bijvoorbeeld: `CalculateTotal()`, `GetCustomerDetails()`.
+## Opmaak
+
+- Beperk regellengte tot **maximaal 100 tekens**.
+- Voeg **lege regels** toe tussen logische secties van de code om de leesbaarheid te verbeteren.
+- Gebruik accolades op een nieuwe regel voor methoden, klassen, enz.
+
+```csharp
+public class MyClass
+{
+    public void MyMethod()
+    {
+        // Code hier
+    }
+}
+
+
--- Blokstart e1/Program.cs
+++ Blokstart e1/Program.cs
@@ -1,32 +1,88 @@
-﻿namespace Blokstart_e1
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Blokstart_e1
 {
 
- using System; 
- using System.Collections.Generic;
 
-internal class Program
+    internal class Program
         {
-            static void Main(string[] args)
-            {
-                Game game = new Game();
+        static void Main()
+        {
+            Game game = new Game();
 
-                game.Start();
+            Console.WriteLine("Dungeon Dude");
+            Console.WriteLine("In the village near your home, there is a dark and mysterious cave.");
+            Console.WriteLine("Legends say it hides a powerful artifact that can bestow incredible power,");
+            Console.WriteLine("but it is guarded by a fearsome dragon and its minions.");
+            Console.WriteLine("Do you want to continue a saved game? (yes/no)");
+            string continueSavedGame = Console.ReadLine().ToLower();
+            if (continueSavedGame == "yes")
+            {
+                game.LoadSavedGame();
             }
+            else if (continueSavedGame == "no")
+            {
+                game.StartNewGame();
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter 'yes' or 'no'.");
+            }
+
+            game.Start();
         }
 
-        class Game
+
+    }
+
+    class Game
         {
             private int playerHp;
             private int playerGold;
             private bool hasBetterWeapon;
             private string currentLocation;
+        private string lastLocation;
+        private bool hasCompletedQuest;
 
-            private Dictionary<string, string> instructions = new Dictionary<string, string>
+
+        private string saveFileName = "saved_game.txt"; 
+
+        public void StartNewGame()
+        {
+            playerHp = 100;
+            playerGold = 0;
+            hasBetterWeapon = false;
+            currentLocation = "start";
+        }
+        public void LoadSavedGame()
+        {
+            if (File.Exists(saveFileName))
+            {
+                using (StreamReader reader = new StreamReader(saveFileName))
+                {
+                    playerHp = int.Parse(reader.ReadLine());
+                    playerGold = int.Parse(reader.ReadLine());
+                    hasBetterWeapon = bool.Parse(reader.ReadLine());
+                    currentLocation = reader.ReadLine();
+                }
+                Console.WriteLine("Loaded saved game.");
+            }
+            else
+            {
+                Console.WriteLine("No saved game found. Starting a new game.");
+                StartNewGame();
+            }
+        }
+        private Dictionary<string, string> instructions = new Dictionary<string, string>
     {
         { "start", "Welcome to Dungeon Dude! You are at your home." },
         { "sleep", "You have recovered your HP." },
         { "shop", "You are at the weapon shop. You can upgrade your gear here." },
-        { "forest", "You are on a forest path. Be prepared to encounter enemies." }
+        { "forest", "You are on a forest path. Be prepared to encounter enemies." },
+        { "cave", "You are in a dark cave. It's a dangerous place with unknown challenges ahead." },
+        { "save", "Save the game so you can return later." }
     };
 
             public Game()
@@ -65,7 +121,13 @@ internal class Program
                         case "forest":
                             ExploreForest();
                             break;
-                        case "quit":
+                    case "cave":
+                        ExploreCave();
+                        break;
+                    case "save": 
+                        SaveGame();
+                        break;
+                    case "quit":
                             Console.WriteLine("Thanks for playing!");
                             Environment.Exit(0);
                             break;
@@ -84,7 +146,17 @@ internal class Program
                 Console.WriteLine($"{instruction.Key} - {instruction.Value}");
             }
         }
-
+        public void SaveGame()
+        {
+            using (StreamWriter writer = new StreamWriter(saveFileName))
+            {
+                writer.WriteLine(playerHp);
+                writer.WriteLine(playerGold);
+                writer.WriteLine(hasBetterWeapon);
+                writer.WriteLine(currentLocation);
+            }
+            Console.WriteLine("Game saved.");
+        }
         private void VisitShop()
         {
             Console.WriteLine("Welcome to the Shop!");
@@ -103,30 +175,20 @@ internal class Program
                 Console.WriteLine("You don't have enough gold to make a purchase. Please come back later. Price: 100 Gold");
             }
         }
-
-        private void ExploreForest()
+        private void SingleEnemyCombat(ref int playerHp, ref int playerGold, bool hasBetterWeapon, int enemyHp)
         {
-            Console.WriteLine("You are on a forest path. As you step forward, you find yourself surrounded by tall, ancient trees. Shafts of dappled sunlight filter through the dense foliage overhead, creating a tranquil and slightly eerie atmosphere. The forest floor is covered in a soft carpet of fallen leaves and moss, muffling your footsteps. The distant sounds of birds and rustling leaves add to the sense of adventure and mystery. You are well aware that danger could lurk around any corner.");
-            Console.WriteLine("You encounter an enemy in the forest!");
-            int enemyHp = 50;
             while (playerHp > 0 && enemyHp > 0)
             {
                 Console.WriteLine($"Your HP: {playerHp} | Enemy HP: {enemyHp}");
                 Console.Write("Choose an action (attack/run): ");
                 string action = Console.ReadLine().ToLower();
 
+                int playerAttackDamage = hasBetterWeapon ? 30 : 20; 
+
                 if (action == "attack")
                 {
-                    if (!hasBetterWeapon)
-                    {
-                        playerHp -= 20;
-                        enemyHp -= 40;
-                    }
-                    else
-                    {
-                        playerHp -= 20;
-                        enemyHp -= 30;
-                    }
+                    playerHp -= 20;
+                    enemyHp -= playerAttackDamage;
 
                     if (enemyHp <= 0)
                     {
@@ -145,12 +207,175 @@ internal class Program
                     Console.WriteLine("Invalid action. Choose 'attack' or 'run'.");
                 }
             }
+
             if (playerHp <= 0)
             {
                 Console.WriteLine("Game over! You were defeated.");
                 Environment.Exit(0);
             }
         }
+
+        private void CaveEnemiesCombat(ref int playerHp, ref int playerGold, bool hasBetterWeapon)
+        {
+            Console.WriteLine("You encounter two menacing figures in the dark cave!");
+            int enemy1Hp = 40;
+            int enemy2Hp = 40;
+
+            while (playerHp > 0 && (enemy1Hp > 0 || enemy2Hp > 0))
+            {
+                Console.WriteLine($"Your HP: {playerHp} | Enemy 1 HP: {enemy1Hp} | Enemy 2 HP: {enemy2Hp}");
+                Console.Write("Choose an action (attack/run): ");
+                string action = Console.ReadLine().ToLower();
+
+                int playerAttackDamage = hasBetterWeapon ? 30 : 20;
+
+                if (action == "attack")
+                {
+                    if (enemy1Hp > 0)
+                        enemy1Hp -= playerAttackDamage;
+
+                    if (enemy2Hp > 0)
+                        enemy2Hp -= playerAttackDamage;
+
+                    playerHp -= 15; 
+
+                    if (enemy1Hp <= 0 && enemy2Hp <= 0)
+                    {
+                        Console.WriteLine("You defeated both enemies!");
+                        playerGold += 50;
+                        break;
+                    }
+                }
+                else if (action == "run")
+                {
+                    Console.WriteLine("You managed to escape from the enemies!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid action. Choose 'attack' or 'run'.");
+                }
+            }
+
+            if (playerHp <= 0)
+            {
+                Console.WriteLine("Game over! You were defeated.");
+                Environment.Exit(0);
+            }
+        }
+
+
+        private void BossEnemyCombat(ref int playerHp, ref int playerGold, bool hasBetterWeapon, int bossEnemyHp)
+        {
+            while (playerHp > 0 && bossEnemyHp > 0)
+            {
+                Console.WriteLine($"Your HP: {playerHp} | Boss Enemy HP: {bossEnemyHp}");
+                Console.Write("Choose an action (attack/run): ");
+                string action = Console.ReadLine().ToLower();
+
+                int playerAttackDamage = hasBetterWeapon ? 50 : 30;
+
+                if (action == "attack")
+                {
+                    playerHp -= 25;
+                    bossEnemyHp -= playerAttackDamage;
+
+                    if (bossEnemyHp <= 0)
+                    {
+                        Console.WriteLine("You defeated the boss enemy!");
+                        playerGold += 100;
+                        break;
+                    }
+                }
+                else if (action == "run")
+                {
+                    Console.WriteLine("You managed to escape from the boss enemy!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid action. Choose 'attack' or 'run'.");
+                }
+            }
+
+            if (playerHp <= 0)
+            {
+                Console.WriteLine("Game over! You were defeated.");
+                Environment.Exit(0);
+            }
+        }
+        private void ExploreForest()
+        {
+            Console.WriteLine("You are on a forest path. As you step forward, you find yourself surrounded by tall, ancient trees. Shafts of dappled sunlight filter through the dense foliage overhead, creating a tranquil and slightly eerie atmosphere. The forest floor is covered in a soft carpet of fallen leaves and moss, muffling your footsteps. The distant sounds of birds and rustling leaves add to the sense of adventure and mystery. You are well aware that danger could lurk around any corner.");
+            Console.WriteLine("You encounter an enemy in the forest!");
+            int enemyHp = 50;
+            int playerAttackDamage = hasBetterWeapon ? 50 : 40; 
+
+            SingleEnemyCombat(ref playerHp, ref playerGold, hasBetterWeapon, enemyHp);
+
+            if (playerHp <= 0)
+            {
+                Console.WriteLine("Game over! You were defeated.");
+                Environment.Exit(0);
+            }
+        }
+
+        private void ExploreCave()
+        {
+            if (hasCompletedQuest)
+            {
+                Console.WriteLine("You have already completed the quest.");
+                return;
+            }
+
+            Console.WriteLine("As you enter the cave, you encounter a massive dragon!");
+            int bossDragonHp = 200; 
+
+            BossEnemyCombat(ref playerHp, ref playerGold, hasBetterWeapon, bossDragonHp);
+
+            bool playerRanFromDragon = playerHp > 0 && bossDragonHp > 0;
+
+            if (playerRanFromDragon)
+            {
+                Console.WriteLine("You decided to run from the fearsome dragon, leaving the cave behind.");
+                Console.WriteLine("Your adventure ends in disgrace as tales of your cowardice spread throughout the village.");
+                Console.WriteLine("Game over! You have not completed the quest.");
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("As you continue deeper into the cave, you discover a hidden chamber with a glowing ancient artifact.");
+
+            Console.Write("Do you want to interact with the ancient artifact? (yes/no): ");
+            string artifactInteraction = Console.ReadLine().ToLower();
+
+            if (artifactInteraction == "yes")
+            {
+                Console.WriteLine("You touch the artifact, and a surge of power flows through you.");
+                Console.WriteLine("Quest completed! You have retrieved the ancient artifact.");
+                hasCompletedQuest = true;
+
+                playerGold += 200; 
+
+                Console.WriteLine("Taking the artifact, you feel its immense power and return to the surface as a hero, known far and wide for your bravery.");
+                Console.WriteLine("Congratulations! You have won the game!");
+            }
+            else if (artifactInteraction == "no")
+            {
+                Console.WriteLine("You decide not to interact with the artifact, leaving it undisturbed in the cave.");
+                Console.WriteLine("You leave the cave, and your adventure continues elsewhere.");
+                Console.WriteLine("Congratulations! You have won the game!");
+            }
+            else
+            {
+                Console.WriteLine("You decide not to interact with the artifact.");
+            }
+        }
+
+
+
+
+
+
 
     }
 }
